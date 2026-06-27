2f16b816a0df7e61f84a7a031e70037f94aaf3df
ssroci <rocissalinas@gmail.com>
2026-06-27 00:34:18 -0300

    actualizacion

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ require('dotenv').config();
 const app = require('./app');
 const connectDB = require('./config/db');
 
-const PORT = process.env.PORT || 4000;
+connectDB();
 
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
-  });
-});
+module.exports = app;
